7df7ed5fcc365df79b450f28cf4d811c744851d0
Dyego Lima <dyegodll18@gmail.com>
2023-11-24 17:12:30 -0300

    Sequencia Fibonacci

--- src/nti/dev/fibonacci/Fibonacci.java
+++ src/nti/dev/fibonacci/Fibonacci.java
@@ -3,8 +3,18 @@ package nti.dev.fibonacci;
 public class Fibonacci {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+//o 3° nummero é a soma dos 2 últimos começando de 0
+		
+        int a = 1, b = 0, aux;
+        
+        System.out.print(b);
+        
+        for(int i = 0; i < 14; i++){
+            System.out.print(", "+a);
+            aux = a;
+            a = aux + b;
+            b = aux; //resultado anterior
+        }
 	}
-
 }
